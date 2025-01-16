ad81ceb05c807948db3f3dc779f2465130e822b6
ysoares0209 <yansoaressilva@outlook.com>
2025-01-16 10:41:18 -0300

    fix: update region in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@
 echo "command - npx tsc"
 npx tsc
 
-echo "command - aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 381491857310.dkr.ecr.us-west-2.amazonaws.com"
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 381491857310.dkr.ecr.us-west-2.amazonaws.com
+echo "command - aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 381491857310.dkr.ecr.us-east-1.amazonaws.com"
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 381491857310.dkr.ecr.us-east-1.amazonaws.com
 
 echo "command - docker build --platform linux/amd64 --progress=plain -t scraper ."
 docker build --platform linux/amd64 --progress=plain -t scraper .
 
-echo "command - docker tag scraper:latest 381491857310.dkr.ecr.us-west-2.amazonaws.com/scraper:latest"
-docker tag scraper:latest 381491857310.dkr.ecr.us-west-2.amazonaws.com/scraper:latest
+echo "command - docker tag scraper:latest 381491857310.dkr.ecr.us-east-1.amazonaws.com/scraper:latest"
+docker tag scraper:latest 381491857310.dkr.ecr.us-east-1.amazonaws.com/scraper:latest
 
-echo "command - docker push 381491857310.dkr.ecr.us-west-2.amazonaws.com/scraper:latest"
-docker push 381491857310.dkr.ecr.us-west-2.amazonaws.com/scraper:latest
+echo "command - docker push 381491857310.dkr.ecr.us-east-1.amazonaws.com/scraper:latest"
+docker push 381491857310.dkr.ecr.us-east-1.amazonaws.com/scraper:latest
